853fd5ca11e3e8074a0eca0a0bc12ee9e35be45f
Hafiz2028 <91659690+Hafiz2028@users.noreply.github.com>
2022-06-14 10:26:40 +0700

    haha

--- inventoris/index_inventori.php
+++ inventoris/index_inventori.php
@@ -1,5 +1,4 @@
 <?php
-include "../config.php";
 
 ?>
 
@@ -10,18 +9,17 @@ include "../config.php";
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>Input Barang - Bima Motor</title>
+  <title>Inventory - Bima Motor</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
   <!-- Favicons -->
   <link href="../assets/img/Logoo.PNG" rel="icon">
-  <link href="../assets/img/Logoo.PNG" rel="Logoo">
-
+  <link href="../assets/img/Logoo.PNG" rel="icon">
   <!-- Google Fonts -->
   <link href="https://fonts.gstatic.com" rel="preconnect">
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
-
+  
   <!-- Vendor CSS Files -->
   <link href="../assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
   <link href="../assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
@@ -30,89 +28,64 @@ include "../config.php";
   <link href="../assets/vendor/quill/quill.bubble.css" rel="stylesheet">
   <link href="../assets/vendor/remixicon/remixicon.css" rel="stylesheet">
   <link href="../assets/vendor/simple-datatables/style.css" rel="stylesheet">
-
+  
   <!-- Template Main CSS File -->
   <link href="../assets/css/style.css" rel="stylesheet">
-
+  
   <!-- =======================================================
   * Template Name: NiceAdmin - v2.2.2
   * Template URL: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/
   * Author: BootstrapMade.com
   * License: https://bootstrapmade.com/license/
   ======================================================== -->
-</head>
-
-<body>
-
+  </head>
+  
+  <body>
+  
   <!-- ======= Header ======= -->
   <header id="header" class="header fixed-top d-flex align-items-center">
-
+  
     <div class="d-flex align-items-center justify-content-between">
-      <a href="index.html" class="logo d-flex align-items-center">
+      <a href="../inventoris/index_inventori.php" class="logo d-flex align-items-center">
         <img src="../assets/img/Logoo.PNG" alt="">
         <span class="d-none d-lg-block">Bima Motor</span>
       </a>
       <i class="bi bi-list toggle-sidebar-btn"></i>
     </div><!-- End Logo -->
   </header><!-- End Header -->
-
-
-  <  <!-- ======= Sidebar ======= -->
+  
+  
+    <!-- ======= Sidebar ======= -->
     <aside id="sidebar" class="sidebar">
   
       <ul class="sidebar-nav" id="sidebar-nav">
   
-      <li class="nav-item">
-          <a class="nav-link " href="../admin/index_admin.php">
+        <li class="nav-item">
+          <a class="nav-link " href="../inventoris/index_inventori.php">
             <i class="bi bi-grid"></i>
             <span>Beranda</span>
           </a>
         </li><!-- End Dashboard Nav -->
   
         <li class="nav-item">
-          <a class="nav-link " href="../admin/view_barang.php">
-            <i class="bi bi-grid"></i>
-            <span>Sparepart</span>
+          <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
+            <i class="bi bi-menu-button-wide"></i><span>Sparepart</span><i class="bi bi-chevron-down ms-auto"></i>
           </a>
-        </li>
-  
-        <li class="nav-item">
-          <a class="nav-link " href="../admin/view_pegawai.php">
-            <i class="bi bi-grid"></i>
-            <span>Pegawai</span>
-          </a>
-        </li>
-
-        <li class="nav-item">
-          <a class="nav-link " href="../admin/view_pelanggan.php">
-            <i class="bi bi-grid"></i>
-            <span>Pelanggan</span>
-          </a>
-        </li>
-  
-        <li class="nav-item">
-          <a class="nav-link collapsed" data-bs-target="#laporan-nav" data-bs-toggle="collapse" href="#">
-            <i class="bi bi-eval-wide"></i><span>Laporan</span><i class="bi bi-chevron-down ms-auto"></i>
-          </a>
-          <ul id="laporan-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+          <ul id="components-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
             <li>
-              <a href="components-alerts.html">
-                <i class="bi bi-circle"></i><span>Pembelian</span>
+              <a href="../inventoris/view_barang-inventori.php">
+                <i class="bi bi-circle"></i><span>Barang</span>
               </a>
             </li>
             <li>
-              <a href="components-accordion.html">
-                <i class="bi bi-circle"></i><span>Pelanggan</span>
-              </a>
-            </li>
-            <li>
-              <a href="components-accordion.html">
-                <i class="bi bi-circle"></i><span>Penjualan</span>
+              <a href="../admin/view_stok.php">
+                <i class="bi bi-circle"></i><span>Stok Barang</span>
               </a>
             </li>
           </ul>
-          </li>
+        </li><!-- End Components Nav -->
   
+        
 
           <li class="nav-item">
           <a class="nav-link collapsed" data-bs-target="#laporan-nav" data-bs-toggle="collapse" href="#">
@@ -121,7 +94,7 @@ include "../config.php";
           <ul id="laporan-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
             <li>
               <a href="components-alerts.html">
-                <i class="bi bi-circle"></i><span>Transaksi</span>
+                <i class="bi bi-circle"></i><span>Stok</span>
               </a>
             </li>
             <li>
@@ -147,107 +120,13 @@ include "../config.php";
   <main id="main" class="main">
 
     <div class="pagetitle">
-      <h1>Barang</h1>
+      <h1>Inventory</h1>
       <nav>
         <ol class="breadcrumb">
-          <li class="breadcrumb-item"><a href="../admin/index_admin.php">Home</a></li>
-          <li class="breadcrumb-item">Sparepart</li>
-          <li class="breadcrumb-item active">Tambah Barang</li>
+          <li class="breadcrumb-item"><a href="dashboard">Home</a></li>
         </ol>
       </nav>
     </div><!-- End Page Title -->
-
-    <?php
-    include "../config.php";
-
-    ?>
-
-
-    <section class="main-panel d-flex">
-      <div class="container card">
-        <div class="section-title">
-          <h3 class="fw-bolder mb-4">Tambah Barang</h3>
-
-        </div>
-
-
-        <form method="POST" class="d-flex">
-          <div class="col-sm-6">
-            <label for="">Id Barang</label>
-            <div class="form-group">
-              <div class="form-line">
-                <input type="text" name="id_barang" class="form-control" maxlength="5" required />
-              </div>
-            </div>
-
-            <label for="">Nama Barang</label>
-            <div class="form-group">
-              <div class="form-line">
-                <input type="text" name="nama_barang" class="form-control" required />
-              </div>
-            </div>
-
-            <label for="">Harga Jual</label>
-            <div class="form-group">
-              <div class="form-line">
-                <input type="text" name="harga_jual" class="form-control" required />
-              </div>
-            </div>
-
-            <label for="">Harga Beli</label>
-            <div class="form-group">
-              <div class="form-line">
-                <input type="text" name="harga_beli" class="form-control" required />
-              </div>
-            </div>
-
-            <label for="">Jumlah Barang</label>
-            <div class="form-group">
-              <div class="form-line">
-                <input type="text" name="total_barang" class="form-control" required />
-              </div>
-            </div>
-
-
-            <input type="submit" name="simpan" value="Simpan" class="but mb-4 w-50 mt-2">
-
-          </div>
-          <div class="col-sm-6">
-
-          </div>
-
-
-      </div>
-
-      </form>
-      <?php
-
-      if (isset($_POST['simpan'])) {
-        $id_barang = $_POST['id_barang'];
-        $nama_barang = $_POST['nama_barang'];
-        $harga_jual = $_POST['harga_jual'];
-        $harga_beli = $_POST['harga_beli'];
-        $total_barang = $_POST['total_barang'];
-
-        $sql = pg_query($conn, "insert into barang (id_barang,nama_barang,harga_jual,harga_beli,total_barang) values ('$id_barang','$nama_barang','$harga_jual', '$harga_beli', '$total_barang')");
-
-        if ($sql) {
-      ?>
-          echo "<script>
-            alert('Data berhasil ditambah');
-            window.location = '../admin/view_barang.php';
-          </script>";
-      <?php
-        }
-      }
-
-      ?>
-
-
-      ?>
-
-      </div>
-    </section>
 
 
   </main><!-- End #main -->
--- inventoris/view_barang-inventori.php
+++ inventoris/view_barang-inventori.php
@@ -1,4 +1,5 @@
-<?php
+<?php  
+include "../config.php";
 
 ?>
 
@@ -9,13 +10,14 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>Transaksi / Stok - Bima Motor</title>
+  <title>Sparepart - Bima Motor</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
   <!-- Favicons -->
   <link href="../assets/img/Logoo.PNG" rel="icon">
-  <link href="../assets/img/Logoo.PNG" rel="icon">
+  <link href="../assets/img/Logoo.PNG" rel="Logoo">
+  
   <!-- Google Fonts -->
   <link href="https://fonts.gstatic.com" rel="preconnect">
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
@@ -46,8 +48,8 @@
   <header id="header" class="header fixed-top d-flex align-items-center">
   
     <div class="d-flex align-items-center justify-content-between">
-      <a href="index.html" class="logo d-flex align-items-center">
-        <img src="../assets/img/Logoo.PNG" alt="">
+      <a href="../admin/index_admin.php" class="logo d-flex align-items-center">
+      <img src="../assets/img/Logoo.PNG" alt="">
         <span class="d-none d-lg-block">Bima Motor</span>
       </a>
       <i class="bi bi-list toggle-sidebar-btn"></i>
@@ -61,7 +63,7 @@
       <ul class="sidebar-nav" id="sidebar-nav">
   
         <li class="nav-item">
-          <a class="nav-link " href="../admin/index_admin.php">
+          <a class="nav-link " href="../inventoris/index_inventori.php">
             <i class="bi bi-grid"></i>
             <span>Beranda</span>
           </a>
@@ -73,7 +75,7 @@
           </a>
           <ul id="components-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
             <li>
-              <a href="../admin/view_barang.php">
+              <a href="../inventoris/view_barang-inventori.php">
                 <i class="bi bi-circle"></i><span>Barang</span>
               </a>
             </li>
@@ -85,53 +87,7 @@
           </ul>
         </li><!-- End Components Nav -->
   
-        <li class="nav-item">
-          <a class="nav-link " href="index_admin.php">
-            <i class="bi bi-grid"></i>
-            <span>Pegawai</span>
-          </a>
-        </li>
-
-        <li class="nav-item">
-          <a class="nav-link collapsed" data-bs-target="#laporan-nav" data-bs-toggle="collapse" href="#">
-            <i class="bi bi-eval-wide"></i><span>Laporan</span><i class="bi bi-chevron-down ms-auto"></i>
-          </a>
-          <ul id="laporan-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
-            <li>
-              <a href="components-alerts.html">
-                <i class="bi bi-circle"></i><span>Pembelian</span>
-              </a>
-            </li>
-            <li>
-              <a href="components-accordion.html">
-                <i class="bi bi-circle"></i><span>Pelanggan</span>
-              </a>
-            </li>
-            <li>
-              <a href="components-accordion.html">
-                <i class="bi bi-circle"></i><span>Penjualan</span>
-              </a>
-            </li>
-          </ul>
-          </li>
-  
-          <li class="nav-item">
-          <a class="nav-link collapsed" data-bs-target="#laporan-nav" data-bs-toggle="collapse" href="#">
-            <i class="bi bi-eval-wide"></i><span>Pelanggan</span><i class="bi bi-chevron-down ms-auto"></i>
-          </a>
-          <ul id="laporan-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
-            <li>
-              <a href="components-alerts.html">
-                <i class="bi bi-circle"></i><span>Data Pelanggan</span>
-              </a>
-            </li>
-            <li>
-              <a href="components-accordion.html">
-                <i class="bi bi-circle"></i><span>Data Mobil Pelanggan</span>
-              </a>
-            </li>
-          </ul>
-          </li>
+        
 
           <li class="nav-item">
           <a class="nav-link collapsed" data-bs-target="#laporan-nav" data-bs-toggle="collapse" href="#">
@@ -166,14 +122,74 @@
   <main id="main" class="main">
 
     <div class="pagetitle">
-      <h1>Dashboard</h1>
+      <h1>Barang</h1>
       <nav>
         <ol class="breadcrumb">
-          <li class="breadcrumb-item"><a href="dashboard">Home</a></li>
+          <li class="breadcrumb-item"><a href="../admin/index_admin.php">Home</a></li>
+          <li class="breadcrumb-item">Sparepart</li>
         </ol>
       </nav>
     </div><!-- End Page Title -->
 
+    <section class="section">
+      <div class="row">
+        <div class="col-lg-12">
+
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Tabel Barang</h5>
+
+              <!-- Default Table -->
+              <table class="table">
+                <thead>
+                  <tr>
+                    <td scope="col">Kode Barang</td>
+                    <td scope="col">Nama Barng</td>
+                    <td scope="col">Harga Jual</td>
+                    <td scope="col">Harga Beli</td>
+                    <td scope="col">Jumlah Barang</td>
+                    
+                  </tr>
+                </thead>
+
+                <tbody>
+                  <?php
+                  $result = pg_query($conn,"SELECT *FROM barang ");
+                  while ($row = pg_fetch_array($result)){
+                    ?>
+                  
+                  <tr>
+                    <td><?=$row['id_barang']?></td>
+                    <td><?=$row['nama_barang']?></td>
+                    <td><?=$row['harga_jual']?></td>
+                    <td><?=$row['harga_beli']?></td>
+                    <td><?=$row['total_barang']?></td>
+                    <td align = "center">
+                    <a class="btn btn-primary" href="../admin/edit_barang.php?id_barang=<?=$row['id_barang']?>">Edit</a>
+                    <a onclick="return confirm('Yakin menghapus data ini ?')" class="btn btn-danger" href="../admin/hapus_barang.php?id_barang=<?=$row['id_barang']?>">Hapus</a>
+                    
+                  </td>
+                  </tr>
+                  <?php
+                  }
+                  ?>
+                <!-- <a href="../admin/input_barang.php" class="button-link">add</a> -->
+                <!-- <button class="btn btn-primary" href="../admin/input_barang.php" >Add</button> -->
+                
+                
+                <button class="btn btn-primary" onclick="location.href='../admin/input_barang.php'" type="button" style="float: right" >Add</button>
+                
+                
+                  
+
+                </tbody>
+              </table>
+              <!-- End Default Table Example -->
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
 
   </main><!-- End #main -->
 
@@ -202,7 +218,6 @@
   <script src="../assets/vendor/simple-datatables/simple-datatables.js"></script>
   <script src="../assets/vendor/tinymce/tinymce.min.js"></script>
   <script src="../assets/vendor/php-email-form/validate.js"></script>
-
   <!-- Template Main JS File -->
   <script src="../assets/js/main.js"></script>
 
